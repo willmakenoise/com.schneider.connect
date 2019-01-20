3ee85c7dd8613fd6a46dff1f1bd3bff2c574759f
willmakenoise <marius@stedjan.com>
2019-01-20 17:23:41 +0100

    Add basic support for mtn508244

--- drivers/mtn508244/device.js
+++ drivers/mtn508244/device.js
@@ -4,10 +4,10 @@ const { ZwaveDevice } = require('homey-meshdriver');
 
 class Schneider508244 extends ZwaveDevice {
 
-	onMeshInit() {
-		this.registerCapability('button', 'SWITCH_REMOTE');
-		this.log('Schneider Move Switch has been inited');
-	}
+  onMeshInit() {
+    // this.registerCapability('button', 'SWITCH_REMOTE');
+    this.log('Schneider Move Switch has been inited');
+  }
 
 }
 
