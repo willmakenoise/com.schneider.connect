21ebbb496257d4e93060e359b4e43f51cead8320
willmakenoise <marius@stedjan.com>
2019-01-20 17:26:16 +0100

    Test dimmer level change

--- drivers/mtn507900/device.js
+++ drivers/mtn507900/device.js
@@ -4,11 +4,25 @@ const { ZwaveDevice } = require('homey-meshdriver');
 
 class Schneider507900 extends ZwaveDevice {
 
-	onMeshInit() {
-		this.registerCapability('dim', 'SWITCH_MULTILEVEL');
-		this.registerCapability('onOff', 'SWITCH_MULTILEVEL');
-		this.log('Schneider Dimmer has been inited');
-	}
+  onMeshInit() {
+    this.enableDebug();
+    this.printNode();
+
+    const settings = this.getSettings();
+
+    this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
+    this.registerCapability('dim', 'SWITCH_MULTILEVEL', {
+    
+    });
+    this.log('Schneider Dimmer has been inited');
+
+    // console.log('Options');
+    // console.log(this.getOptions());
+
+    console.log('Dim Duration:');
+    console.log(settings.dimming_duration);
+
+  }
 
 }
 
