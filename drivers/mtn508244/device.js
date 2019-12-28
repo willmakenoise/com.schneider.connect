87c08094f0cd86b9715f62f4828b12fb6dec8a4d
willmakenoise <marius@stedjan.com>
2019-12-28 01:43:32 +0100

    Work on Move Switch

--- drivers/mtn508244/device.js
+++ drivers/mtn508244/device.js
@@ -4,40 +4,73 @@ const { ZwaveDevice } = require('homey-meshdriver');
 
 class Schneider508244 extends ZwaveDevice {
 
-  async onMeshInit() {
+  onMeshInit() {
     this.enableDebug();
-    this.printNode();
+    // this.printNode();
     // this.registerCapability('button', 'SWITCH_REMOTE');
+
+    this.registerReportListener('BASIC', 'BASIC_GET', ( rawReport, parsedReport ) => {
+			console.log('\nReceived BASIC_GET from Move Switch');
+      // console.log('RawReport:', rawReport);
+      // console.log('ParsedReport: ', parsedReport);
+
+      let bs = new Buffer([0xA2]);
+      // let bs = {
+      //   'Value': new Buffer([0xA1])
+      // }
+
+      // this.node.CommandClass.COMMAND_CLASS_BASIC
+      //   .BASIC_REPORT(bs)
+      //     .catch(this.error)
+      //     .then(res => {
+      //       this.log("Res: ", res);
+      //     })
+
+      this.node.CommandClass.COMMAND_CLASS_SWITCH_MULTILEVEL
+        .SWITCH_MULTILEVEL_START_LEVEL_CHANGE(bs)
+          .then(res => {
+            this.log("Res: ", res);
+          })
+          .catch(this.error)
+
+		});
 
     this.registerReportListener('CONFIGURATION', 'CONFIGURATION_REPORT', ( rawReport, parsedReport ) => {
 			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
 		});
     this.registerReportListener('SWITCH_MULTILEVEL', 'SWITCH_MULTILEVEL_REPORT', ( rawReport, parsedReport ) => {
-			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
+			console.log('SWITCH_MULTILEVEL_REPORT', rawReport, parsedReport);
 		});
     this.registerReportListener('BASIC_WINDOW_COVERING', 'BASIC_WINDOW_COVERING_START_LEVEL_CHANGE', ( rawReport, parsedReport ) => {
-			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
+			console.log('BASIC_WINDOW_COVERING_START_LEVEL_CHANGE', rawReport, parsedReport);
 		});
     this.registerReportListener('BASIC_WINDOW_COVERING', 'BASIC_WINDOW_COVERING_STOP_LEVEL_CHANGE', ( rawReport, parsedReport ) => {
-			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
+			console.log('BASIC_WINDOW_COVERING_STOP_LEVEL_CHANGE', rawReport, parsedReport);
 		});
     this.registerReportListener('SWITCH_MULTILEVEL', 'SWITCH_MULTILEVEL_START_LEVEL_CHANGE', ( rawReport, parsedReport ) => {
-			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
+			console.log('SWITCH_MULTILEVEL_START_LEVEL_CHANGE', rawReport, parsedReport);
 		});
     this.registerReportListener('SWITCH_MULTILEVEL', 'SWITCH_MULTILEVEL_STOP_LEVEL_CHANGE', ( rawReport, parsedReport ) => {
-			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
+			console.log('SWITCH_MULTILEVEL_STOP_LEVEL_CHANGE', rawReport, parsedReport);
 		});
     this.registerReportListener('MULTI_INSTANCE_ASSOCIATION', 'MULTI_INSTANCE_ASSOCIATION_REPORT', ( rawReport, parsedReport ) => {
-			console.log('CONFIGURATION_REPORT', rawReport, parsedReport);
+			console.log('MULTI_INSTANCE_ASSOCIATION_REPORT', rawReport, parsedReport);
 		});
+    // this.registerReportListener('', '', ( rawReport, parsedReport ) => {
+		// 	console.log('Other', rawReport, parsedReport);
+		// });
 
     this.log('Schneider Move Switch has been inited');
 
 
     // await this.configurationSet({index: 60, size: 1}, true);
 
-    await this.node.CommandClass.COMMAND_CLASS_CONFIGURATION
-      .CONFIGURATION_GET();
+    // await this.node.CommandClass.COMMAND_CLASS_MULTI_INSTANCE_ASSOCIATION
+    //   .MULTI_INSTANCE_ASSOCIATION_GET();
+    // await this.node.CommandClass.COMMAND_CLASS_MULTI_INSTANCE_ASSOCIATION
+    //   .MULTI_INSTANCE_ASSOCIATION_GROUPINGS_GET();
+    // await this.node.CommandClass.COMMAND_CLASS_CONFIGURATION
+    //   .CONFIGURATION_GET();
 
     // this.configurationGet({index: 4}).then( res => {
     //   console.log(res);
--- drivers/mtn508244/driver.js
+++ drivers/mtn508244/driver.js
@@ -6,13 +6,13 @@ class Schneider508244Driver extends Homey.Driver {
     onInit() {
         super.onInit();
 
-        this.onButtonTrigger = new Homey.FlowCardTriggerDevice('mtn508244').register().registerRunListener((args, state) => {
-          console.log('args:');
-          console.log(args);
-          console.log('state');
-          console.log(state);
-          return args.device.buttonRunListener(args, state);
-        });
+        // this.onButtonTrigger = new Homey.FlowCardTriggerDevice('mtn508244').register().registerRunListener((args, state) => {
+        //   console.log('args:');
+        //   console.log(args);
+        //   console.log('state');
+        //   console.log(state);
+        //   return args.device.buttonRunListener(args, state);
+        // });
     }
 }
 
